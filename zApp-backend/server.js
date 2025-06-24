69875225ec4e6ea2e7faac9f924219dfcd3ae5e0
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-06-24 21:06:37 +1200

    feat: set up database connection

--- zApp-backend/server.js
+++ zApp-backend/server.js
@@ -9,13 +9,34 @@ import dotenv from 'dotenv';
 import stationLocationsRoutes from "./routes/stationLocations.js";
 
 dotenv.config();
+const app = express()
 const PORT = process.env.PORT;
-
+const MongoDB = process.env.MONGO_URI
+mongoose.Promise = global.Promise;
 
 
 //___ DATABASE CONNECTION ___
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MongoDB);
+        console.log("MongoDB Connected");
 
+        process.on('exit', async () => {
+            if (mongoose.connection.readyState === 1) {
+                await closeDB();
+                console.log("MongoDB Connection Closed");
+            }
+        });
 
+    } catch (err) {
+        console.error("Connection Error:", err.message);
+        process.exit(1);
+    }
+};
+
+const closeDB = async () => {
+  await mongoose.connection.close();
+};
 
 //___ MIDDLEWARES ___
 app.use(express.json());
@@ -26,6 +47,11 @@ app.use(cors('http://localhost:5173'))
 
 
 //___ API ROUTES ___
+//___ TEST ___
+app.get('/test', (req, res) => {
+  res.status(200).json({ message: "API is working!" });
+});
+
 
 // This is for displaying station locations on the map (Erekle)
 app.use("/api/stations", stationLocationsRoutes);
@@ -33,3 +59,5 @@ app.use("/api/stations", stationLocationsRoutes);
 
 //___ SERVER CONNECTION ___
 app.listen(PORT, () => console.log(`Server is connected at http://localhost:${PORT}`))
+
+export { connectDB, closeDB}
