5e204a83cf0c4199ad017c3fb657da50771866de
Sonny Tapara <logifixit@gmail.com>
2025-06-28 17:30:06 +1200

    integrated map, updated styling

--- zApp-frontend/src/App.jsx
+++ zApp-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./components/common/Footer.jsx";
 import MainBody from "./components/common/MainBody.jsx";
 import NavbarTop from "./components/common/NavBarTop.jsx";
+import NavBarUnderTop from "./components/common/NavBarUnderTop.jsx";
 
 
 function App() {
   return (
     <div>
       <NavbarTop />
+      <NavBarUnderTop />
       <MainBody />
       <Footer />
       
